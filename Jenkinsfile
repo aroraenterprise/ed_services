8131314ef23ebb6d908dccfae2277c405f6ba2c8
Saj Arora <saj.arora24@gmail.com>
2019-02-20 17:20:38 -0500

    updated jenkinsfile and script for store build

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,9 +1,12 @@
 pipeline {
   agent any
+  tools {
+    nodejs 'node'
+  }
   stages {
     stage('Build Store') {
       steps {
-        sh 'java -jar ./swagger-codegen-cli-2.4.0.jar generate -i ./store.yaml -l typescript-angular -o ./clients/store --additional-properties ngVersion=6'
+        sh 'npm run deploy-store'
       }
     }
   }
--- update-module-ts.js
+++ update-module-ts.js
@@ -2,26 +2,34 @@
 
 const fs = require('fs');
 const latestVersion = require('latest-version');
-require('dotenv').config();
+const yargs = require('yargs');
 
-let npmModuleFile = fs.readFileSync('./clients/package-ts.json');  
-let ngPackage = fs.readFileSync('./clients/ng-package.json');  
-let packageJson = JSON.parse(npmModuleFile); 
-packageJson['name'] = process.env.TsServiceName;
+async function getLatestService(dirName, serviceName) {
+  let npmModuleFile = fs.readFileSync('./clients/package-ts.json');
+  let ngPackage = fs.readFileSync('./clients/ng-package.json');
+  let packageJson = JSON.parse(npmModuleFile);
+  console.log('Building ' + serviceName);
+  packageJson['name'] = serviceName;
+  let version = null;
+  try {
+    version = await latestVersion(serviceName);
+    let versionParts = version.split('.');
+    version = `${versionParts[0]}.${versionParts[1]}.${parseInt(versionParts[2]) + 1}`;
+  } catch (err) {
+    version = "1.0.0";
+  }
 
-async function getLatestService() {
-    let version = null;
-    try {
-        version = await latestVersion(process.env.TsServiceName);
-        let versionParts = version.split('.');
-        version = `${versionParts[0]}.${versionParts[1]}.${parseInt(versionParts[2]) + 1}`;
-    } catch(err) {
-        version = "1.0.0";
-    }
-
-    packageJson['version'] = version;
-    fs.writeFileSync('./clients/typescript/package.json', JSON.stringify(packageJson, null, 4));
-    fs.writeFileSync('./clients/typescript/ng-package.json', ngPackage);
+  packageJson['version'] = version;
+  fs.writeFileSync(`./ts/${dirName}/package.json`, JSON.stringify(packageJson, null, 4));
+  fs.writeFileSync(`./ts/${dirName}/ng-package.json`, ngPackage);
 }
 
-getLatestService();
+yargs
+  .command(
+    'deploy-store',
+    'Deploy the store',
+    {},
+    () => getLatestService('store', '@edropin/store-ts'),
+  )
+  .wrap(120)
+  .help().argv;
